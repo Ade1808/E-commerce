b323eb886fc7b7f077f55a77b96d11c3ec1dd58f
Ade1808 <a.rasaki.07@gmail.com>
2019-02-18 19:22:44 +0000

    Password Rsppec Added and checked

--- spec/requests/paswword_resets_spec.rb
+++ spec/requests/paswword_resets_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "PaswwordResets", type: :request do
-  describe "GET /paswword_resets" do
-    it "works! (now write some real specs)" do
-      get paswword_resets_path
-      expect(response).to have_http_status(200)
-    end
-  end
+ 	it "emails user when requesting password reset " do
+		user = Factory(:user)
+		visit login_path
+		click_link "password"
+		fill_in "Email" , :with => user.email 
+		click_button "Reset Password"
+	end
 end
